6f1b35f9a1e06ff9d3d6820a8cd899f9cc684f15
khuunsh68 <goncalofarias10@ua.pt>
2023-03-14 14:40:37 +0000

    Updates feitas!

---
+++ src/main/java/com/mycompany/gitrepositorio/GitRepositorio.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gitrepositorio;
+
+/**
+ * teste de commits
+ * @author Lenovo
+ */
+public class GitRepositorio {
+
+    public static void main(String[] args) {
+        int result = 4 + 4;
+        System.out.println("Ola maltinha!");
+        System.out.println("Sou o Goncalo!");
+        //commit teste
+        //commit 2
+        System.out.println(result);
+    }
+}
